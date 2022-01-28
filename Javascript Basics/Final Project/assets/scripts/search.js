56bbe979408e8084dd7780a5c1a1f57a9fd72eda
Nazarchamp <42648833+Nazarchamp@users.noreply.github.com>
2022-01-28 17:19:20 -0600

    Add files via upload

--- Javascript Basics/Final Project/assets/scripts/search.js
+++ Javascript Basics/Final Project/assets/scripts/search.js
@@ -3,10 +3,20 @@ const querryInput = document.getElementById('query');
 const googleBtn = document.getElementById('google-btn');
 const bingBtn = document.getElementById('bing-btn');
 
+const searchErr = document.getElementById('large-error-p');
+
 googleBtn.addEventListener('click', ()=>{
+    if(querryInput.value.trim() === ""){
+        searchErr.style.display = "block";
+        return;
+    }
    window.location.href = `https://google.com/search?q=${querryInput.value.trim()}`;
 });
 
 bingBtn.addEventListener('click', ()=>{
+    if(querryInput.value.trim() === ""){
+        searchErr.style.display = "block";
+        return;
+    }
     window.location.href = `https://bing.com/search?q=${querryInput.value.trim()}`;
 });
